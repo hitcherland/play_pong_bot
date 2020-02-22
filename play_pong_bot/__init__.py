51e227963c368a7b08451962a3788a32de436989
Cian Booth <thisiscian@gmail.com>
2020-02-22 18:57:04 +0000

    now using twitter labs to read poll apis

---
+++ play_pong_bot/__init__.py
@@ -0,0 +1,3 @@
+'''A twitter bot that lets users play PONG using polls'''
+
+from . import bot
---
+++ start_bot.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''Commandline interface for the play_pong_bot class'''
+
+import os
+from play_pong_bot.bot import TwitterBot
+
+ENV_PREFIX = 'PLAY_PONG_BOT'
+
+def main():
+    '''The main function, this should run once per update'''
+    consumer_key = os.environ.get(f'{ENV_PREFIX}_CONSUMER_KEY')
+    consumer_secret = os.environ.get(f'{ENV_PREFIX}_CONSUMER_SECRET')
+    token_key = os.environ.get(f'{ENV_PREFIX}_TOKEN_KEY')
+    token_secret = os.environ.get(f'{ENV_PREFIX}_TOKEN_SECRET')
+
+    bot = TwitterBot(consumer_key, consumer_secret, token_key, token_secret)
+
+    tweet = bot.get_last_tweet()
+    print(tweet.full_text)
+    print(tweet.card_uri)
+    print(tweet.entities)
+
+if __name__ == '__main__':
+    main()
